f643b4de59a9880c875a58b0c76eca8cd9759afa
Dickson Daniel Peprah <techpepson@gmail.com>
2026-07-07 13:39:03 +0000

    update(prisma file): update the prisma.service file to add the database url to avoid the issue of env file not being seen in docs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,7 +6,9 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export class PrismaService extends PrismaClient {
   constructor() {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString:
+        process.env.DATABASE_URL ||
+        'postgresql://neondb_owner:npg_dYHsnEo6vW0z@ep-rough-dust-ahz42sza-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
     });
     super({ adapter });
   }
